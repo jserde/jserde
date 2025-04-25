63732d5f59c50c9c97d272fd0917f08275b6801e
Laurent Pireyn <laurent@pireyn.be>
2025-04-25 12:52:34 +0200

    feat(jserde-json-ser): Create JSON serialization components

---
+++ jserde-json-ser/src/main/java/jserde/json/ser/JsonValueWriter.java
@@ -0,0 +1,388 @@
+/*
+ * Copyright 2025 JSerde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jserde.json.ser;
+
+import com.google.errorprone.annotations.ForOverride;
+import com.google.errorprone.annotations.MustBeClosed;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import jserde.core.ser.DataMapWriter;
+import jserde.core.ser.DataSequenceWriter;
+import jserde.core.ser.DataStructWriter;
+import jserde.core.ser.DataValueWriter;
+import jserde.core.ser.SerializationException;
+import jserde.core.ser.ValueSerializer;
+import jserde.io.AbstractWriter;
+import jserde.json.JsonFormat;
+import org.jspecify.annotations.Nullable;
+
+/**
+ * JSON {@link DataValueWriter}.
+ *
+ * @author Laurent Pireyn
+ */
+public final class JsonValueWriter implements DataValueWriter {
+    // TODO #improvement: Make max nesting depth configurable
+    private static final int MAX_NESTING_DEPTH = 100;
+
+    private final class JsonStringWriter extends AbstractWriter {
+        // TODO: Consider using java.util.HexFormat
+        private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+        private boolean closed;
+
+        void reopen() throws IOException {
+            closed = false;
+            writer.write('"');
+        }
+
+        @Override
+        public void write(int c) throws IOException {
+            if (closed) {
+                throw new IOException("Writer closed");
+            }
+            switch (c) {
+                case '"', '\\' -> {
+                    writer.write('\\');
+                    writer.write(c);
+                }
+                case '\n' -> {
+                    writer.write('\\');
+                    writer.write('n');
+                }
+                case '\r' -> {
+                    writer.write('\\');
+                    writer.write('r');
+                }
+                case '\t' -> {
+                    writer.write('\\');
+                    writer.write('t');
+                }
+                case '\f' -> {
+                    writer.write('\\');
+                    writer.write('f');
+                }
+                case '\b' -> {
+                    writer.write('\\');
+                    writer.write('b');
+                }
+                // NOTE: Slash ('/') is not escaped
+                default -> {
+                    if (c < ' ') {
+                        // Control character
+                        writer.write('\\');
+                        writer.write('u');
+                        writer.write(HEX_DIGITS[(c & 0xf000) >> 12]);
+                        writer.write(HEX_DIGITS[(c & 0x0f00) >> 8]);
+                        writer.write(HEX_DIGITS[(c & 0x00f0) >> 4]);
+                        writer.write(HEX_DIGITS[c & 0x000f]);
+                    } else {
+                        // Non-control character
+                        writer.write(c);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            // TODO: Since we know this writer will be closed exactly once, we don't need the `closed` flag
+            if (!closed) {
+                try {
+                    writer.write('"');
+                } finally {
+                    closed = true;
+                }
+            }
+        }
+    }
+
+    private abstract sealed class JsonContainerWriter implements Closeable permits JsonSequenceWriter, JsonStructWriter {
+        private boolean closed;
+
+        @MustBeClosed
+        JsonContainerWriter() throws IOException {
+            writeContainerBegin();
+        }
+
+        @ForOverride
+        abstract void writeContainerBegin() throws IOException;
+
+        @Override
+        public final void close() throws IOException {
+            if (!closed) {
+                try {
+                    writeContainerEnd();
+                } finally {
+                    closed = true;
+                    afterCloseContainerWriter();
+                }
+            }
+        }
+
+        @ForOverride
+        abstract void writeContainerEnd() throws IOException;
+    }
+
+    private final class JsonSequenceWriter extends JsonContainerWriter implements DataSequenceWriter {
+        private int index;
+
+        @MustBeClosed
+        JsonSequenceWriter() throws IOException {}
+
+        @Override
+        void writeContainerBegin() throws IOException {
+            writer.write('[');
+        }
+
+        @Override
+        public <T extends @Nullable Object> void serializeElement(T value, ValueSerializer<? super T> serializer) throws IOException {
+            if (index > 0) {
+                writer.write(',');
+            }
+            serializer.serializeValue(value, JsonValueWriter.this);
+            ++index;
+        }
+
+        @Override
+        void writeContainerEnd() throws IOException {
+            writer.write(']');
+        }
+    }
+
+    private final class JsonStructWriter extends JsonContainerWriter implements DataStructWriter {
+        private int index;
+
+        @MustBeClosed
+        JsonStructWriter() throws IOException {}
+
+        @Override
+        void writeContainerBegin() throws IOException {
+            writer.write('{');
+        }
+
+        @Override
+        public <T extends @Nullable Object> void serializeField(String name, T value, ValueSerializer<? super T> serializer) throws IOException {
+            if (index > 0) {
+                writer.write(',');
+            }
+            serializeString(name);
+            writer.write(':');
+            serializer.serializeValue(value, JsonValueWriter.this);
+            ++index;
+        }
+
+        @Override
+        void writeContainerEnd() throws IOException {
+            writer.write('}');
+        }
+    }
+
+    private final Writer writer;
+
+    // NOTE: Number formats are instance fields because NumberFormat is not thread-safe
+    // TODO #optimization: Find a more efficient way to format floating-point numbers
+
+    /**
+     * {@link DecimalFormat} used for {@code float} values.
+     */
+    private final DecimalFormat floatFormat = new DecimalFormat("0.##########", DecimalFormatSymbols.getInstance(Locale.ROOT));
+
+    /**
+     * {@link DecimalFormat} used for {@code double} values.
+     */
+    private final DecimalFormat doubleFormat = new DecimalFormat("0.####################", DecimalFormatSymbols.getInstance(Locale.ROOT));
+
+    /**
+     * Reusable JSON string {@link Writer}.
+     */
+    private final JsonStringWriter stringWriter = new JsonStringWriter();
+
+    /**
+     * Current nesting depth.
+     */
+    private int nestingDepth;
+
+    /**
+     * Creates a new {@code JsonValueWriter} on the given {@link Writer}.
+     *
+     * @param writer the writer
+     */
+    // NOTE: This is not annotated with @MustBeClosed, as not all Writer subclasses require to be closed
+    public JsonValueWriter(Writer writer) {
+        this.writer = writer;
+    }
+
+    /**
+     * Creates a new {@code JsonValueWriter} on the given {@link OutputStream},
+     * using {@link JsonFormat#DEFAULT_CHARSET}.
+     *
+     * @param output the output stream
+     */
+    // NOTE: This is not annotated with @MustBeClosed, as not all Writer subclasses require to be closed
+    public JsonValueWriter(OutputStream output) {
+        this(new OutputStreamWriter(output, JsonFormat.DEFAULT_CHARSET));
+    }
+
+    // DataValueWriter methods
+
+    @Override
+    public void serializeNull() throws IOException {
+        writer.write("null");
+    }
+
+    @Override
+    public void serializeBoolean(boolean value) throws IOException {
+        writer.write(value ? "true" : "false");
+    }
+
+    @Override
+    public void serializeByte(byte value) throws IOException {
+        writer.write(String.valueOf(value));
+    }
+
+    @Override
+    public void serializeShort(short value) throws IOException {
+        writer.write(String.valueOf(value));
+    }
+
+    @Override
+    public void serializeInt(int value) throws IOException {
+        writer.write(String.valueOf(value));
+    }
+
+    @Override
+    public void serializeLong(long value) throws IOException {
+        writer.write(String.valueOf(value));
+    }
+
+    @Override
+    public void serializeBigInteger(BigInteger value) throws IOException {
+        writer.write(value.toString());
+    }
+
+    @Override
+    public void serializeFloat(float value) throws IOException {
+        writer.write(floatFormat.format(value));
+    }
+
+    @Override
+    public void serializeDouble(double value) throws IOException {
+        writer.write(doubleFormat.format(value));
+    }
+
+    @Override
+    public void serializeBigDecimal(BigDecimal value) throws IOException {
+        writer.write(value.toString());
+    }
+
+    @Override
+    public void serializeChar(char value) throws IOException {
+        try (var writer = openStringWriter()) {
+            writer.write(value);
+        }
+    }
+
+    @Override
+    public void serializeString(String value) throws IOException {
+        try (var writer = openStringWriter()) {
+            writer.write(value);
+        }
+    }
+
+    @Override
+    public void serializeString(Reader reader, int lengthHint) throws IOException {
+        try (
+            reader;
+            var writer = openStringWriter()
+        ) {
+            reader.transferTo(writer);
+        }
+    }
+
+    @Override
+    public void serializeByteArray(byte[] value) throws IOException {
+        // TODO: Implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @MustBeClosed
+    public DataSequenceWriter serializeSequence(int size) throws IOException {
+        return openSequenceWriter();
+    }
+
+    @Override
+    @MustBeClosed
+    // TODO: Don't suppress MustBeClosedChecker when it supports this flow
+    @SuppressWarnings("MustBeClosedChecker")
+    public DataMapWriter serializeMap(int size) throws IOException {
+        return openStructWriter().asMapWriter();
+    }
+
+    @Override
+    @MustBeClosed
+    public DataStructWriter serializeStruct(int fieldCount) throws IOException {
+        return openStructWriter();
+    }
+
+    @Override
+    public void close() throws IOException {
+        writer.close();
+    }
+
+    // Child writers-related methods
+
+    @MustBeClosed
+    private JsonStringWriter openStringWriter() throws IOException {
+        stringWriter.reopen();
+        return stringWriter;
+    }
+
+    private void beforeOpenContainerWriter() throws SerializationException {
+        if (nestingDepth == MAX_NESTING_DEPTH) {
+            throw new SerializationException("The maximum nesting depth of " + MAX_NESTING_DEPTH + " has been reached");
+        }
+        ++nestingDepth;
+    }
+
+    private void afterCloseContainerWriter() {
+        --nestingDepth;
+        assert nestingDepth >= 0;
+    }
+
+    @MustBeClosed
+    private JsonSequenceWriter openSequenceWriter() throws IOException {
+        beforeOpenContainerWriter();
+        return new JsonSequenceWriter();
+    }
+
+    @MustBeClosed
+    private JsonStructWriter openStructWriter() throws IOException {
+        beforeOpenContainerWriter();
+        return new JsonStructWriter();
+    }
+}
---
+++ jserde-json-ser/src/test/java/jserde/json/ser/JsonValueWriterTests.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2025 JSerde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jserde.json.ser;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import jserde.core.ser.ValueSerializer;
+import jserde.core.ser.standard.StandardBigDecimalSerializer;
+import jserde.core.ser.standard.StandardBigIntegerSerializer;
+import jserde.core.ser.standard.StandardBooleanSerializer;
+import jserde.core.ser.standard.StandardByteSerializer;
+import jserde.core.ser.standard.StandardCharSerializer;
+import jserde.core.ser.standard.StandardDoubleSerializer;
+import jserde.core.ser.standard.StandardFloatSerializer;
+import jserde.core.ser.standard.StandardIntSerializer;
+import jserde.core.ser.standard.StandardLongSerializer;
+import jserde.core.ser.standard.StandardNullSerializer;
+import jserde.core.ser.standard.StandardShortSerializer;
+import jserde.core.ser.standard.StandardStringSerializer;
+import jserde.test.AbstractTests;
+import org.jspecify.annotations.Nullable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class JsonValueWriterTests extends AbstractTests {
+    <T extends @Nullable Object> String serializeValue(T value, ValueSerializer<? super T> serializer) throws IOException {
+        final var stringWriter = new StringWriter();
+        try (
+            stringWriter;
+            var valueWriter = new JsonValueWriter(stringWriter)
+        ) {
+            serializer.serializeValue(value, valueWriter);
+        }
+        final var json = stringWriter.toString();
+        log("JSON: " + json);
+        return json;
+    }
+
+    @Test
+    void testSerializeNull() throws IOException {
+        assertEquals("null", serializeValue(null, StandardNullSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "true, 'true'",
+        "false, 'false'",
+    })
+    void testSerializeBoolean(boolean value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardBooleanSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, '0'",
+        "1, '1'",
+        "-1, '-1'",
+        Byte.MAX_VALUE + ", '" + Byte.MAX_VALUE + '\'',
+        Byte.MIN_VALUE + ", '" + Byte.MIN_VALUE + '\'',
+    })
+    void testSerializeByte(byte value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardByteSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, '0'",
+        "1, '1'",
+        "-1, '-1'",
+        Short.MAX_VALUE + ", '" + Short.MAX_VALUE + '\'',
+        Short.MIN_VALUE + ", '" + Short.MIN_VALUE + '\'',
+    })
+    void testSerializeShort(short value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardShortSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, '0'",
+        "1, '1'",
+        "-1, '-1'",
+        Integer.MAX_VALUE + ", '" + Integer.MAX_VALUE + '\'',
+        Integer.MIN_VALUE + ", '" + Integer.MIN_VALUE + '\'',
+    })
+    void testSerializeInt(int value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardIntSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, '0'",
+        "1, '1'",
+        "-1, '-1'",
+        Long.MAX_VALUE + ", '" + Long.MAX_VALUE + '\'',
+        Long.MIN_VALUE + ", '" + Long.MIN_VALUE + '\'',
+    })
+    void testSerializeLong(long value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardLongSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, '0'",
+        "1, '1'",
+        "-1, '-1'",
+        "12345678901234567890123456789012345678901234567890, '12345678901234567890123456789012345678901234567890'",
+        "-12345678901234567890123456789012345678901234567890, '-12345678901234567890123456789012345678901234567890'",
+    })
+    void testSerializeBigInteger(BigInteger value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardBigIntegerSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0.0, '0'",
+        "1.0, '1'",
+        "-1.0, '-1'",
+        "0.5, '0.5'",
+        "-0.5, '-0.5'",
+        "1.5, '1.5'",
+        "-1.5, '-1.5'",
+    })
+    void testSerializeFloat(float value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardFloatSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0.0, '0'",
+        "1.0, '1'",
+        "-1.0, '-1'",
+        "0.5, '0.5'",
+        "-0.5, '-0.5'",
+        "1.5, '1.5'",
+        "-1.5, '-1.5'",
+    })
+    void testSerializeDouble(double value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardDoubleSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, '0'",
+        "1, '1'",
+        "-1, '-1'",
+        "12345678901234567890123456789012345678901234567890, '12345678901234567890123456789012345678901234567890'",
+        "-12345678901234567890123456789012345678901234567890, '-12345678901234567890123456789012345678901234567890'",
+        "12345678901234567890123456789012345678901234567890.0, '12345678901234567890123456789012345678901234567890.0'",
+        "-12345678901234567890123456789012345678901234567890.0, '-12345678901234567890123456789012345678901234567890.0'",
+        "0.12345678901234567890123456789012345678901234567890, '0.12345678901234567890123456789012345678901234567890'",
+        "-0.12345678901234567890123456789012345678901234567890, '-0.12345678901234567890123456789012345678901234567890'",
+        "12345678901234567890123456789012345678901234567890.12345678901234567890123456789012345678901234567890, '12345678901234567890123456789012345678901234567890.12345678901234567890123456789012345678901234567890'",
+        "-12345678901234567890123456789012345678901234567890.12345678901234567890123456789012345678901234567890, '-12345678901234567890123456789012345678901234567890.12345678901234567890123456789012345678901234567890'",
+    })
+    void testSerializeBigDecimal(BigDecimal value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardBigDecimalSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'a', '\"a\"'",
+        "'\n', '\"\\n\"'",
+        "'\r', '\"\\r\"'",
+        "'\t', '\"\\t\"'",
+        "'\b', '\"\\b\"'",
+        "'\f', '\"\\f\"'",
+        "'\u0001', '\"\\u0001\"'",
+    })
+    void testSerializeChar(char value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardCharSerializer.INSTANCE));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'', '\"\"'",
+        "'a', '\"a\"'",
+        "'abc', '\"abc\"'",
+        "'a b c', '\"a b c\"'",
+        "'\\\"\n\r\t\b\f\u0001', '\"\\\\\\\"\\n\\r\\t\\b\\f\\u0001\"'",
+    })
+    void testSerializeString(String value, String json) throws IOException {
+        assertEquals(json, serializeValue(value, StandardStringSerializer.INSTANCE));
+    }
+}
